e52e767c8fd1191afdc8226097afcef4427753d9
EL BAHJA Mohamed <elbahjamohamed0@gmail.com>
2021-03-12 16:50:37 +0100

    Add content to register page

--- register.php
+++ register.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register Page</title>
 </head>
+
 <body>
     <h1>Register Page</h1>
+    <form action="" method="post">
+        <div>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email">
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password">
+        </div>
+
+        <input type="submit" value="Sign in">
+    </form>
 </body>
+
 </html>
